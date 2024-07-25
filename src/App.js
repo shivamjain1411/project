3e635ff8ba71b08d850ee98803f5789a101b5e8e
shivamjain1411 <realshivamjain@gmail.com>
2024-07-25 21:20:44 +0530

    added a line

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello</div>;
+  return <div className="App">hello world this is my project</div>;
 }
 
 export default App;
