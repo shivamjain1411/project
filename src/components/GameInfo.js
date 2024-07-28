f5e2d73aaa0cf3ced50ebb58525967bfc2c7b1b7
shivamjain1411 <realshivamjain@gmail.com>
2024-07-28 09:03:13 +0530

    final game page

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -1,48 +1,92 @@
 import React from "react";
-import gameimg from "../Images/live_game_3.jpeg";
+import PropTypes from "prop-types"; // Import PropTypes for type checking
 import { FaCrown } from "react-icons/fa";
-function GameInfo() {
+import img from "../Images/live_game_3.jpeg";
+function GameInfo({
+  gameName,
+  bettingOpenTime,
+  bettingCloseTime,
+  lastWinnerNumber,
+  royalWinnerText,
+  yellowColor,
+}) {
   return (
     <div>
       <div className="game-info bg-gradient-to-b to-[#140934] from-[#35188c] rounded-xl flex justify-between">
         <div className="game-details flex p-8">
           <img
-            src={gameimg}
+            src={img}
             alt="Game"
-            className="game-image h-50 w-44 rounded-2xl border-2 border-yellow-400"
+            className="game-image h-50 w-44 rounded-2xl border-2"
+            style={{ borderColor: yellowColor }}
           />
           <div className="pt-3 px-2">
             <div className="game-name pb-3">
-              <p className="font-bold text-white">Game Name</p>{" "}
-              <h1 className="font-bold text-lg text-yellow-400">TAJ</h1>
+              <p className="font-bold text-white">Game Name</p>
+              <h1 className="font-bold text-lg" style={{ color: yellowColor }}>
+                {gameName}
+              </h1>
             </div>
             <div className="bet-time pb-3">
-              <p className="font-bold text-white">Betting Close Time</p>
-              <h1 className="font-bold text-lg text-yellow-400"> 5:40 PM</h1>
+              <p className="font-bold text-white">Betting Open Time</p>
+              <h1 className="font-bold text-lg" style={{ color: yellowColor }}>
+                {bettingOpenTime}
+              </h1>
             </div>
             <div className="pb-3">
               <p className="font-bold text-white">Betting Close Time</p>
-              <h1 className="font-bold text-lg text-yellow-400"> 6:30 PM</h1>
+              <h1 className="font-bold text-lg" style={{ color: yellowColor }}>
+                {bettingCloseTime}
+              </h1>
             </div>
           </div>
         </div>
-        <div className="winner-info flex flex-col justify-center items-center p-12 w-6/12 ">
-          <div className="last-winner bg-yellow-400 rounded-full w-32 border h-32">
+        <div className="winner-info flex flex-col justify-center items-center p-12 w-6/12">
+          <div
+            className="last-winner rounded-full w-32 border h-32"
+            style={{ backgroundColor: yellowColor }}
+          >
             <div className="winner-number flex flex-col justify-center items-center">
               <div className="relative top-4 left-6 rotate-12 ">
                 <FaCrown style={{ color: "white" }} />
               </div>
-              <h1 className="text-6xl text-white font-bold">93</h1>
+              <h1 className="text-6xl text-white font-bold">
+                {lastWinnerNumber}
+              </h1>
               <div className="winner-text text-white">Last Winner</div>
             </div>
           </div>
-          <div className="royal-winner text-xl mt-2 font-bold text-yellow-400">
-            The Royal Winner
+          <div
+            className="royal-winner text-xl mt-2 font-bold"
+            style={{ color: yellowColor }}
+          >
+            {royalWinnerText}
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+// Set default props
+GameInfo.defaultProps = {
+  gameName: "TAJ",
+  bettingOpenTime: "5:40 PM",
+  bettingCloseTime: "6:30 PM",
+  lastWinnerNumber: "93",
+  royalWinnerText: "The Royal Winner",
+  yellowColor: "#FFD700", // Default yellow color (Gold)
+  image: "../Images/live_game_3.jpeg", // Default image path
+};
+
+// Define prop types for better type checking and documentation
+GameInfo.propTypes = {
+  gameName: PropTypes.string,
+  bettingOpenTime: PropTypes.string,
+  bettingCloseTime: PropTypes.string,
+  lastWinnerNumber: PropTypes.string,
+  royalWinnerText: PropTypes.string,
+  yellowColor: PropTypes.string,
+};
 
 export default GameInfo;
--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -5,8 +5,23 @@ import GameBody from "../components/GameBody";
 function GamePage() {
   return (
     <div className="bg-[#140934] p-8">
-      <GameInfo />
-      <GameBody />
+      <GameInfo
+        gameName="TAJ"
+        bettingOpenTime="5:40 PM"
+        bettingCloseTime="6:30 PM"
+        lastWinnerNumber="93"
+        royalWinnerText="The Royal Winner"
+        yellowColor="#FFD700" // Use any color you want here
+      />
+      <GameBody
+        initialTotalAmount={500}
+        maxAmount={5000}
+        numbers={Array.from({ length: 100 }, (_, i) => i)} // Pass numbers
+        primaryColor="#FFD700" // Gold
+        hoverColor="#FFA500" // Orange
+        headerTitles={["Jodi", "Harup", "Crossing"]}
+        betButtonText="Place a Bet"
+      />
     </div>
   );
 }
